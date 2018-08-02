71ee5af7ca18834bb4c3809b6e93ff7458f33711
Andrew Chambers <andrewchambers@fastmail.com>
2018-08-02 20:33:11 +1200

    more useful io functions

--- extraio.go
+++ extraio.go
@@ -3,7 +3,12 @@ package extraio
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
+	"net"
+	"os/exec"
 	"strconv"
+	"sync/atomic"
+	"time"
 )
 
 type MergedReadWriteCloser struct {
@@ -109,4 +114,73 @@ func minInt(a, b int) int {
 		return a
 	}
 	return b
+}
+
+// Sets cmd.Stderr to io.Discard
+// sets cmd.Stdout and cmd.Stdin to pipes connected
+// to the returned read write closer.
+func CmdReadWriteCloser(cmd *exec.Cmd) io.ReadWriteCloser {
+	a, b := io.Pipe()
+	x, y := io.Pipe()
+
+	cmd.Stderr = ioutil.Discard
+	cmd.Stdout = b
+	cmd.Stdin = x
+
+	rwc := &MergedReadWriteCloser{
+		RC: a,
+		WC: y,
+	}
+
+	return rwc
+}
+
+type MeteredConn struct {
+	Conn net.Conn
+	// if accessed concurrently, Read with sync/atomic
+	ReadCount int64
+	// if accessed concurrently, Read with sync/atomic
+	WriteCount int64
+}
+
+func NewMeteredConn(c net.Conn) *MeteredConn {
+	return &MeteredConn{
+		Conn: c,
+	}
+}
+
+func (mConn *MeteredConn) Read(buf []byte) (int, error) {
+	n, err := mConn.Conn.Read(buf)
+	atomic.AddInt64(&mConn.ReadCount, int64(n))
+	return n, err
+}
+
+func (mConn *MeteredConn) Write(buf []byte) (int, error) {
+	n, err := mConn.Conn.Write(buf)
+	atomic.AddInt64(&mConn.WriteCount, int64(n))
+	return n, err
+}
+
+func (mConn *MeteredConn) Close() error {
+	return mConn.Conn.Close()
+}
+
+func (mConn *MeteredConn) LocalAddr() net.Addr {
+	return mConn.Conn.LocalAddr()
+}
+
+func (mConn *MeteredConn) RemoteAddr() net.Addr {
+	return mConn.Conn.RemoteAddr()
+}
+
+func (mConn *MeteredConn) SetDeadline(t time.Time) error {
+	return mConn.Conn.SetDeadline(t)
+}
+
+func (mConn *MeteredConn) SetReadDeadline(t time.Time) error {
+	return mConn.Conn.SetReadDeadline(t)
+}
+
+func (mConn *MeteredConn) SetWriteDeadline(t time.Time) error {
+	return mConn.Conn.SetWriteDeadline(t)
 }
